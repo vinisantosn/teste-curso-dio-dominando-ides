4a67e4e25f1b31c163350f724ece0540cc014528
Vinícius Santos <vinisantoosk@gmail.com>
2022-03-02 21:33:34 -0300

    primeiro commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,22 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+
+		System.out.println(gato);
+		System.out.println(livros);
+
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+}
